c4ba04504d7d995a96bae38b8c90123c97aeb7a2
Ahsan45 <ashahab@stevens.edu>
2018-09-18 17:14:52 -0400

    Add files via upload

--- test_githubAPI.py
+++ test_githubAPI.py
@@ -1,9 +1,10 @@
+# will fail if api limit is reached
 import unittest
 import Shahab_Ahsan_HW4a
 
 class TestGitHubAPI(unittest.TestCase):
     def test_Ahsan45(self):
-        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('Ahsan45'), 117)
+        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('Ahsan45'), 118)
 
     def test_DavidKim15(self):
         self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('DavidKim15'), 41)
