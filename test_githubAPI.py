66e451cb5196a63ba000eba068ccf9ae6d86801f
Ahsan45 <ashahab@stevens.edu>
2018-09-18 16:56:23 -0400

    Add files via upload

--- test_githubAPI.py
+++ test_githubAPI.py
@@ -3,10 +3,10 @@ import Shahab_Ahsan_HW4a
 
 class TestGitHubAPI(unittest.TestCase):
     def test_Ahsan45(self):
-        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('Ahsan45'), 116)
+        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('Ahsan45'), 117)
 
-    def test_richkempinski(self):
-        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('richkempinski'), 39)
+    def test_DavidKim15(self):
+        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('DavidKim15'), 41)
 
 if __name__ == '__main__':
     unittest.main()
