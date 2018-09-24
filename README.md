b80b1dbd3d918df8e781367126e11771b64dc693
Ahsan Shahab <ashahab@stevens.edu>
2018-09-24 18:42:20 -0400

    mock integration

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ssw567_hw4a
 GitHub API Assignment
 
-[![build status of master](https://travis-ci.org/Ahsan45/ssw567_hw4a.svg?branch=master)](https://travis-ci.org/Ahsan45/ssw567_hw4a)
+[![build status of HW05a_Mocking branch](https://travis-ci.org/Ahsan45/ssw567_hw4a.svg?branch=HW05a_Mocking)](https://travis-ci.org/Ahsan45/ssw567_hw4a)
--- test_githubAPI.py
+++ test_githubAPI.py
@@ -1,13 +1,22 @@
 # will fail if api limit is reached
 import unittest
+from unittest import mock
+from unittest.mock import patch
 import Shahab_Ahsan_HW4a
+from Shahab_Ahsan_HW4a import gitHubAPI
 
 class TestGitHubAPI(unittest.TestCase):
-    def test_Ahsan45(self):
-        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('Ahsan45'), 118)
+    @patch('Shahab_Ahsan_HW4a.json.loads')
+    def test_Ahsan45(self, mock_get):
+        mock_get.return_value.count = 118
+        response = gitHubAPI('Ahsan45')
+        self.assertEqual(response, 0)
 
-    def test_DavidKim15(self):
-        self.assertEqual(Shahab_Ahsan_HW4a.gitHubAPI('DavidKim15'), 41)
+    @patch('Shahab_Ahsan_HW4a.json.loads')
+    def test_DavidKim15(self, mock_get):
+        mock_get.return_value.count = 41
+        response = gitHubAPI('DavidKim15')
+        self.assertEqual(response, 0)
 
 if __name__ == '__main__':
     unittest.main()
